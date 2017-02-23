08f18b996097ee863b30d8d7330fbf800e6381fd
Amol Gaikwad <amol@Amols-Mac.local>
2017-02-23 10:51:07 -0500

    Initial Commit

---
+++ MeaningsApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MeaningsApp
+//
+//  Created by Amol Gaikwad on 2/23/17.
+//  Copyright © 2017 Amol Gaikwad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
